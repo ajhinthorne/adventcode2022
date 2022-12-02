6bd4f2803b6e0cdd1c20e8549f6f052f380560ae
ajhinthorne <adam.j.hinthorne@gmail.com>
2022-12-01 16:15:12 -0800

    New Code Files

---
+++ 2022_12_01/dayone.py
@@ -0,0 +1,11 @@
+###dayone problem: https://adventofcode.com/2022/day/1
+###Problem: Retrieve 50 stars
+
+# %% import packages
+import pandas as pd
+import numpy as np
+
+
+# %%
+elf_list = pd.read_csv("C:\Users\Adam Hinthorne\Desktop\Advent2022Local\adventcode2022\2022_12_01\dayone_input.txt",delim_whitespace=True)
+elf_list.head()
